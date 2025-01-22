2889312ea71e3d5eb5635e01316c3c8a2feecec0
sunilkalluru1332 <sunil.kalluru@riversideinsights.com>
2025-01-22 18:50:55 +0530

    multi org credentials and chage in the cidata and auth

--- src/tests/auth.setup.ts
+++ src/tests/auth.setup.ts
@@ -1,10 +1,9 @@
 import { test as setup } from "../base/basePageFixtures";
 import { getWj5UserData } from "../utils/testData";
+import { getWj5CIUserData } from "../utils/ciTestData";
 
 const ahFile = "src/.auth/ah.json";
 const examinerFile = "src/.auth/examiner.json";
-const ahData = getWj5UserData().accountHolder[0];
-const examinerData = getWj5UserData().examiner[0];
 
 setup.describe.configure({ mode: "serial" });
 
@@ -16,16 +15,27 @@ setup("Examiner login", async ({ context, page, wj5loginPage }) => {
   await examineeLogin(context, page, wj5loginPage);
 });
 
+function getUserData() {
+  if (process.env.CI === "true" && process.env.SHARD_NUMBER) {
+    return getWj5CIUserData(parseInt(process.env.SHARD_NUMBER));
+  }
+  return getWj5UserData();
+}
+
 async function ahLogin(context, page, wj5loginPage) {
+  const userData = getUserData();
+  const ahData = userData.accountHolder[0];
   await wj5loginPage.loginToRiversideScore(ahData.userName, ahData.passWord);
   await page.context().storageState({ path: ahFile });
   await context.close();
 }
 
 async function examineeLogin(context, page, wj5loginPage) {
+  const userData = getUserData();
+  const examinerData = userData.examiner[0];
   await wj5loginPage.loginToRiversideScore(
     examinerData.userName,
-    examinerData.passWord,
+    examinerData.passWord
   );
   await page.context().storageState({ path: examinerFile });
   await context.close();
