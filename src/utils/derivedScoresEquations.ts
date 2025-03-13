13e66fe4e9ea94b95200810b71ef786befbb8e44
sunilkalluru1332 <sunil.kalluru@riversideinsights.com>
2025-03-14 01:13:12 +0530

    reveret changes back

--- src/utils/derivedScoresEquations.ts
+++ src/utils/derivedScoresEquations.ts
@@ -531,7 +531,8 @@ export function getConfidenceRange(
     if (computedValue >= 39.5 && computedValue <= 160.5) {
       // console.log(computedValue);
       // return String(Math.round(computedValue));
-      return String(Math.round(applyRoundToEvenRule(computedValue)));
+      // return String(Math.round(applyRoundToEvenRule(computedValue)));
+      return String(Math.round(Number(applyRoundToEvenRule(computedValue).toFixed(2))));
     } else if (computedValue < 39.5) {
       return "<40";
     } else if (computedValue > 160.5) {
